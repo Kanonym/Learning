a4fd5053b9e0c28bc55bf9a70495c8239fdcf3b3
Kanonym <keyanoush@gmx.de>
2016-12-18 14:43:58 -0800

    moving around

--- Learning_mac/Learning/main.cpp
+++ Learning_mac/Learning/main.cpp
@@ -5,13 +5,32 @@
 //  Created by Keyanoush Razavi on 16.12.16.
 //  Copyright © 2016 Kanonymous. All rights reserved.
 //
-
+#include <fstream>
+#include <string>
 #include <iostream>
+
+#define PI 3.14159
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::string hallo;
-    std::cout << "Hello, World!\n";
-    std::cin >> hallo;
+    string hallo;
+    cout << "Hello, World!\n";
+    
+    ifstream myfile;
+    
+    myfile.open("hello.txt");
+    
+    while(myfile >> hallo)
+    {
+        cout << hallo;
+    }
+    cout << endl;
+    
+    double dRadius = 3.0;
+    double dArea = PI * dRadius * dRadius;
+
+    
     return 0;
 }
